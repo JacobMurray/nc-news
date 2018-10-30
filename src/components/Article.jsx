fa96e5cc1a96fad67161db6c10f96ed6edef56e3
Jacob <jacob_murray@hotmail.co.uk>
2018-10-30 14:38:02 +0000

    put single article on article page

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -3,33 +3,34 @@ import PropTypes from 'prop-types';
 import * as api from '../api';
 
 class Article extends Component {
-    state = {
-        article : {}
-    }
-    render() {
-        return (
-            <div>
-                selected Article
-            </div>
-        );
-    }
+  state = {
+    article: {}
+  };
+  render() {
+    const { belongs_to, body, created_by, title, votes } = this.state.article;
+    return (
+      <div>
+        <h3>{title}</h3>
+        <p>{body}</p>
+        <h4>Belongs to: {belongs_to}</h4>
+        <h4>votes: {votes}</h4>
+      </div>
+    );
+  }
 
-    componentDidMount () {
-        this.fetchArticle()
-    }
+  componentDidMount() {
+    this.fetchArticle();
+  }
 
-    fetchArticle = () => {
-        api.getArticleById(this.props.article_id)
-        .then(article => this.setState({
-            article
-        }))
-    }
+  fetchArticle = () => {
+    api.getArticleById(this.props.article_id).then(article =>
+      this.setState({
+        article
+      })
+    );
+  };
 }
 
-
-
-Article.propTypes = {
-
-};
+Article.propTypes = {};
 
 export default Article;
