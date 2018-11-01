b4a9983a28a9db4ae5b57b01b8739f1fc6ebb2ed
Jacob <jacob_murray@hotmail.co.uk>
2018-11-01 12:37:11 +0000

    create delete comment

--- src/components/DeleteComment.jsx
+++ src/components/DeleteComment.jsx
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import * as api from '../api';
 
-const DeleteComment = props => {
-    return (
-        <div>
-            <button onClick={api.deleteComment(props.id)}>Delete Comment</button>
-        </div>
-    );
-};
+class DeleteComment extends Component {
+    render() {
+        return (
+            <div>
+                <button onClick={() => this.handleClick()}>Delete Comment</button>
+            </div>
+        );
+    }
+
+    handleClick = () => {
+        api.deleteComment(this.props.id)
+        this.forceUpdate()
+    }
+}
 
 DeleteComment.propTypes = {
     id: PropTypes.string
